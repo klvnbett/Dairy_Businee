9a6ab1de297cd336d8a995a301f4e2abd659c933
Moringa School <kelvin.bett@student.moringaschool.com>
2021-05-02 08:01:30 +0300

    variable declaration to js

---
+++ production.js
@@ -0,0 +1,14 @@
+// variable declaration
+var shedA;
+var shedB;
+var shedC;
+var shedD;
+
+var sA;
+var sB;
+var sC;
+var sD;
+
+var sheds = [{ name: 'shedA' }, { name: 'shedB' },
+    { name: 'shedC' }, { name: 'shedD' }
+]
