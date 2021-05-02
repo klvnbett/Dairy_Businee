08c6d0dd57bde49494e4ccc7f51d1aa0e2ed0fe2
Moringa School <kelvin.bett@student.moringaschool.com>
2021-05-02 08:18:04 +0300

    function assignment and arguments

--- milkProduction.js
+++ milkProduction.js
@@ -12,3 +12,16 @@ var sD;
 var sheds = [{ name: 'shedA' }, { name: 'shedB' },
     { name: 'shedC' }, { name: 'shedD' }
 ]
+
+function dailyProduce() {
+    shedA = document.getElementById('shedA').value;
+    shedB = document.getElementById('shedB').value;
+    shedC = document.getElementById('shedC').value;
+    shedD = document.getElementById('shedD').value;
+
+    sA = parseFloat(shedA);
+    sB = parseFloat(shedB);
+    sC = parseFloat(shedC);
+    sD = parseFloat(shedD);
+
+}
