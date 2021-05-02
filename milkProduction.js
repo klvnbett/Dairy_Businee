47affadfdbfd7373907535bcc90fd53ffe211f78
Moringa School <kelvin.bett@student.moringaschool.com>
2021-05-02 08:54:05 +0300

    array calling

--- milkProduction.js
+++ milkProduction.js
@@ -26,6 +26,15 @@ function dailyProduce() {
 
     var shedsArray = [sA, sB, sC, sD];
     var totalDaily = shedsArray[0] + shedsArray[1] + shedsArray[2] + shedsArray[3];
+    if (isNaN(sA) || isNaN(sB) || isNaN(sC) || isNaN(sD)) {
+        alert(" records should not be empty");
+    } else {
+        document.getElementById("shedsRecord").innerHTML = "production per shed."
+        document.getElementById("shed_A").innerHTML = "Your Production In Shed A is " + shedsArray[0] + "liters per day"
+        document.getElementById("shed_B").innerHTML = "Your Production In Shed B is " + shedsArray[0] + "liters per day"
+        document.getElementById("shed_C").innerHTML = "Your Production In Shed C is " + shedsArray[0] + "liters per day"
+        document.getElementById("shed_D").innerHTML = "Your Production In Shed D is " + shedsArray[0] + "liters per day"
+    }
 
 
 }
