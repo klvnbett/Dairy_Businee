7bd2645b96ee46e2b889bc54cd33264873849c78
Moringa School <kelvin.bett@student.moringaschool.com>
2021-05-02 09:34:12 +0300

    function to calculate the price

--- milkProduction.js
+++ milkProduction.js
@@ -30,11 +30,23 @@ function dailyProduce() {
         alert(" records should not be empty");
     } else {
         document.getElementById("shedsRecord").innerHTML = "production per shed."
-        document.getElementById("shed_A").innerHTML = "Your Production In Shed A is " + shedsArray[0] + "liters per day"
-        document.getElementById("shed_B").innerHTML = "Your Production In Shed B is " + shedsArray[0] + "liters per day"
-        document.getElementById("shed_C").innerHTML = "Your Production In Shed C is " + shedsArray[0] + "liters per day"
-        document.getElementById("shed_D").innerHTML = "Your Production In Shed D is " + shedsArray[0] + "liters per day"
+        document.getElementById("shed_A").innerHTML = "Your Production In Shed A is " + shedsArray[0] + "liters per day."
+        document.getElementById("shed_B").innerHTML = "Your Production In Shed B is " + shedsArray[0] + "liters per day."
+        document.getElementById("shed_C").innerHTML = "Your Production In Shed C is " + shedsArray[0] + "liters per day."
+        document.getElementById("shed_D").innerHTML = "Your Production In Shed D is " + shedsArray[0] + "liters per day."
+        document.getElementById("total").innerHTML = "Your Total Production in all Sheds is" + totalDaily + "liters per day."
+
     }
+}
 
+function calculate() {
+    shedA = document.getElementById('shed_A').value;
+    shedB = document.getElementById('shed_B').value;
+    shedC = document.getElementById('shed_C').value;
+    shedD = document.getElementById('shed_D').value;
 
+    sA = parseFloat(shedA);
+    sB = parseFloat(shedB);
+    sC = parseFloat(shedC);
+    sD = parseFloat(shedD);
 }
